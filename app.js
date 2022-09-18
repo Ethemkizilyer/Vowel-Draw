210bfa8c4d400f0cf6eb1d9b59e90356ad71508c
Ethem <ethemkizilyer3546@gmail.com>
2022-09-19 00:49:07 +0300

    Update

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const p = document.querySelector(".ünlü");
 const ert = document.querySelector(".ünsüz");
 const qwe = document.querySelector(".toplam");
 // const mn = document.querySelector(".kelime");
-const elf = document.querySelector("#elf")
+const sil = document.querySelector("#sil")
 
 
-counter = 0;
-coun = 0;
-kelime = 1;
+let counter = 0;
+let coun = 0;
 let a;
 let asd = [
   "a",
@@ -31,22 +30,9 @@ let asd = [
 let sessiz =["q","w","r","t","y","p","ğ","ş","l","k","j","h","g","f","d","s","z","x","c","v","b","n","m","ç"]
 
 console.log(input.value); 
-// input.addEventListener("input", (event) =>{
-// input.value.split("").forEach((e) => {
-//   if (asd.includes(e)) {
-//     counter++;
-//   } else if (sessiz.includes(e)) {
-//     coun++;
-//   } else if (e.code == "Space") {
-//     kelime++;
-//   }
-
-//   // p.innerText = counter;
-//   // ert.innerText = coun;
-// });
-// })
 input.addEventListener("input", () => {
- 
+let counter = 0;
+let coun = 0;
   if (input.value.length > 1) {
     elf.addEventListener("click", () => {
       console.log(input.value.length);
@@ -55,62 +41,38 @@ input.addEventListener("input", () => {
           counter++;
         } else if (sessiz.includes(e)) {
           coun++;
-        } else if (e.code == "Space") {
-          kelime++;
-        }
+        } 
       });
+   
       p.innerText = "Sesli Harf Sayısı: " + counter;
       ert.innerText = "Sessiz Harf Sayısı: " + coun;
       qwe.innerText = "Toplam Harf Sayısı: " + (coun + counter);
+      
     });
+
   } else {
      if (asd.includes(input.value)) {
        counter++;
      } else if (sessiz.includes(input.value)) {
        coun++;
      }
+    
      p.innerText = "Sesli Harf Sayısı: " + counter;
       ert.innerText = "Sessiz Harf Sayısı: " + coun;
       qwe.innerText = "Toplam Harf Sayısı: " + (coun + counter);
+     
      console.log(input.value);
     input.addEventListener("keydown", (event) => {
-      // console.log(event.code);
-      // console.log(input.value);
-      if (event.code == "Space") {
-        ++kelime;
-      }
-      // console.log(event.key);
+      
       asd.includes(event.key) == true
         ? counter++
         : sessiz.includes(event.key.toLowerCase()) == true
         ? coun++
         : a;
-      // *******************************************
-      // if (asd.includes(input.value.split("")[input.value.split("").length - 1])) {
-      //   --counter;
-      // } else if (
-      //   sessiz.includes(input.value.split("")[input.value.split("").length - 1])
-      // ) {
-      //   --coun;
-      // }
-      // input.value.split("").forEach((e) => {
-      //   if (asd.includes(e)) {
-      //     counter++;
-      //   } else if (sessiz.includes(e)) {
-      //     coun++;
-      //   } else if (e.code == "Space") {
-      //     kelime++;
-      //   }
-
-      //   p.innerText = counter;
-      //   ert.innerText = coun;
-      // });
-      // *******************************************
-
+       
       if (event.key == "Backspace") {
         counter = 0;
         coun = 0;
-        // kelime= 0
 
         if (
           asd.includes(input.value.split("")[input.value.split("").length - 1])
@@ -134,16 +96,29 @@ input.addEventListener("input", () => {
 
           p.innerText = counter;
           ert.innerText = coun;
+           
         });
       }
 
-      //  mn.innerText = "Kelime Sayısı: "+kelime;
       p.innerText = "Sesli Harf Sayısı: " + counter;
       ert.innerText = "Sessiz Harf Sayısı: " + coun;
       qwe.innerText = "Toplam Harf Sayısı: " + (coun + counter);
+      
+       
     });
   }
+  
+  
 });
+ sil.addEventListener("click", () => {
+   input.value = "";
+   counter = 0;
+   coun = 0;
+   p.innerText = "Sesli Harf Sayısı: " + 0;
+   ert.innerText = "Sessiz Harf Sayısı: " + 0;
+   qwe.innerText = "Toplam Harf Sayısı: " + 0;
+ });
+
 // *************************************************************
 
 const num = document.querySelector("#num");
@@ -156,9 +131,9 @@ num.addEventListener("input", (e) => {
 
   let asd = "*";
   let b = " ";
-  let qw = 4;
+ 
   let n = a;
-  // let a = 5;
+  
   let m = 0;
   let str = "";
   if (a == 0) {
@@ -167,7 +142,7 @@ num.addEventListener("input", (e) => {
     text.innerHTML = str += b.repeat(n) + asd.repeat(1) + b.repeat(n);
   } else if (a > 1) {
     str += b.repeat(n - 1) + asd.repeat(1) + b.repeat(n - 1);
-    // console.log(b.repeat(n-1) + asd.repeat(1) + b.repeat(n-1));
+    
 
     str += "\n";
     for (let i = 0; i < a - 1; i++) {
@@ -179,17 +154,11 @@ num.addEventListener("input", (e) => {
         asd.repeat(1) +
         b.repeat(n - 1) +
         "\n";
-      // console.log(
-      //   b.repeat(n - 1) +
-      //     asd.repeat(1) +
-      //     b.repeat(2 * m + 1) +
-      //     asd.repeat(1) +
-      //     b.repeat(n - 1)
-      // );
+  
       m += 1;
     }
     n = 0;
-    // console.log(n);
+   
     for (let j = 0; j < a - 2; j++) {
       m -= 1;
       str +=
@@ -199,18 +168,12 @@ num.addEventListener("input", (e) => {
         asd.repeat(1) +
         b.repeat(n + 1) +
         "\n";
-      //  console.log(
-      //   b.repeat(n + 1) +
-      //     asd.repeat(1) +
-      //     b.repeat(2 * m - 1) +
-      //     asd.repeat(1) +
-      //     b.repeat(n + 1)
-      // );
+   
 
       n += 1;
     }
     str += b.repeat(n + 1) + asd.repeat(1) + b.repeat(n + 1) + "\n";
-    // console.log(b.repeat(n + 1) + asd.repeat(1) + b.repeat(n + 1));
+  
     text.innerHTML = str;
   } else {
     text.innerHTML = "Lütfen Pozitif Sayı Giriniz";
